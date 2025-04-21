a634faffc89e9944071ab336382baa3de44e7d3e
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-04-21 18:38:31 -0500

    create display info functions

--- index.js
+++ index.js
@@ -1,45 +1,105 @@
 const cardContainer = document.querySelector("#card-container");
+const daily = document.querySelector("#daily");
+const weekly = document.querySelector("#weekly");
+const monthly = document.querySelector("#monthly");
+
+
+daily.addEventListener("click", displayDaily);
+weekly.addEventListener("click", displayWeekly);
+monthly.addEventListener("click", displayMonthly);
 
 const fetchData = async () => {
     try {
         const response = await fetch("data.json");
         if (!response.ok) {
-            switch (response.status) {
-                case 404:
-                    throw new Error('Data file not found');
-                case 403:
-                    throw new Error('Access to data file forbidden');
-                default:
-                    throw new Error(`HTTP error! status: ${response.status}`);
-            }
+            throw new Error("Could not retrieve data")
         }
         const data = await response.json();
         return data;
     } catch(error) {
-        throw new Error('Error fetching data:', error);
+        alert(error.message);
     }
 }
 
-async function initialize() {
+async function displayDaily() {
     let data = await fetchData();
+    let element = "";
 
     console.log(data);
 
     data.forEach((activity) => {
-        cardContainer.innerHTML += 
-        `<section class="card" id="${activity.title}">
+        element += 
+        `<section class="card" id="${activity.title.toLowerCase().replace(" ", "-")}">
         <div id="content">
           <div class="content-top">
             <h2 class="activity-title">${activity.title}</h2>
             <img src="images/icon-ellipsis.svg" alt="ellipsis">
           </div>
           <div class="content-bottom">
-            <p class="current">${activity.timeframes.daily.current}</p>
-            <p class="prev">Yesterday - ${activity.timeframes.daily.previous}</p>
+            <p class="current">${activity.timeframes.daily.current}hrs</p>
+            <p class="prev">Yesterday - ${activity.timeframes.daily.previous}hrs</p>
           </div>
         </div>
       </section>`
     })
+    cardContainer.innerHTML = element;
+    daily.classList.add("white");
+    weekly.classList.remove("white");
+    monthly.classList.remove("white")
 }
 
-initialize();
+async function displayWeekly() {
+    let data = await fetchData();
+    let element = "";
+
+    console.log(data);
+
+    data.forEach((activity) => {
+        element += 
+        `<section class="card" id="${activity.title.toLowerCase().replace(" ", "-")}">
+        <div id="content">
+          <div class="content-top">
+            <h2 class="activity-title">${activity.title}</h2>
+            <img src="images/icon-ellipsis.svg" alt="ellipsis">
+          </div>
+          <div class="content-bottom">
+            <p class="current">${activity.timeframes.weekly.current}hrs</p>
+            <p class="prev">Last week - ${activity.timeframes.weekly.previous}hrs</p>
+          </div>
+        </div>
+      </section>`
+    })
+    cardContainer.innerHTML = element;
+    daily.classList.remove("white");
+    weekly.classList.add("white");
+    monthly.classList.remove("white")
+}
+
+async function displayMonthly() {
+    let data = await fetchData();
+    let element = "";
+
+    console.log(data);
+
+    data.forEach((activity) => {
+        element += 
+        `<section class="card" id="${activity.title.toLowerCase().replace(" ", "-")}">
+        <div id="content">
+          <div class="content-top">
+            <h2 class="activity-title">${activity.title}</h2>
+            <img src="images/icon-ellipsis.svg" alt="ellipsis">
+          </div>
+          <div class="content-bottom">
+            <p class="current">${activity.timeframes.monthly.current}hrs</p>
+            <p class="prev">Last month - ${activity.timeframes.monthly.previous}hrs</p>
+          </div>
+        </div>
+      </section>`
+    })
+    cardContainer.innerHTML = element;
+    daily.classList.remove("white");
+    weekly.classList.remove("white");
+    monthly.classList.add("white")
+}
+
+displayDaily();
